76a1264861c7d235af3f98ff97590bf873e1bd8e
darealsammy <160411460+darealsammy@users.noreply.github.com>
2024-10-26 21:58:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const text = document.getElementById('text');
-
-function handleScroll() {
-    const rect = text.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
-
-    // Check if the text is in view
-    if (rect.top < windowHeight && rect.bottom > 0) {
-        text.classList.add('visible');
-        window.removeEventListener('scroll', handleScroll); // Remove event listener after fade-in
-    }
-}
-
-window.addEventListener('scroll', handleScroll);
 
